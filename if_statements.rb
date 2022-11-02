e36c3b2cdae35b3aeeb1940ab92f0cb4a082bbb0
James Taylor <jrtaylor0614@gmail.com>
2022-11-02 13:43:46 -0600

    makes both quarter conditionsable to be achieved

--- if_statements.rb
+++ if_statements.rb
@@ -10,7 +10,7 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-weather = "sunny"
+weather = "rainy"
 
 if weather == "sunny"
   p "sunscreen"
@@ -43,9 +43,11 @@ end
 
 num_quarters = 0
 
+if num_quarters == 1
 puts "I have enough money for a gumball"
+elsif num_quarters <= 0
 puts "I don't have enough money for a gumball"
-
+end
 
 #####################
 # Using the variables defined below, write code that will tell you
