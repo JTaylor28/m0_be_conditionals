e14f5714c373ce97da81ef20113a24965423d408
James Taylor <jrtaylor0614@gmail.com>
2022-11-02 12:02:23 -0600

    Completed part 1

--- comparisons.rb
+++ comparisons.rb
@@ -17,29 +17,36 @@ number_students = 20
 string_teachers = "4"
 numberDogs = 0
 
-# EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
+# EXAMPLE: print the result of the comparison: is number_teachers greater than number_students?
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts number_students <= 21
 # this should print: true
 
 
@@ -52,53 +59,64 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 # Make sure YOU can explain it that way!
 
-puts 4 < 9
-#YOU DO: Explain.
-
-books = 3
-puts 4 < books
-# YOU DO: Explain.
-
-friends = 6
-siblings = 2
-puts friends > siblings
-# YOU DO: Explain.
-
-attendees = 9
-meals = 8
-puts attendees != meals
-# YOU DO: Explain.
 
 
-#-------------------
-# PART 3: Logical Operators
-#-------------------
-
-is_hungry = true
-finished_homework = false
-
-# EXAMPLE:
-# Determine if the user is hungry and has completed their homework
-puts is_hungry && finished_homework
-# Determine if the user is hungry or has completed their homework
-puts is_hungry || finished_homework
-
-loves_to_play = true
-loves_dog_park = false
-loves_treats = true
-age = 1
-
-# YOU DO:
-# Determine if the dog loves to play and loves treats
 
 
-# Determine if the dog loves to play and loves the dog park
 
 
-# Determine if the dog loves to play or loves the dog park
 
 
-# Determine if the dog loves to play and is a puppy
 
-# What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+
+#-------------------------------------
+# puts 4 < 9
+# #YOU DO: Explain.
+
+# books = 3
+# puts 4 < books
+# # YOU DO: Explain.
+
+# friends = 6
+# siblings = 2
+# puts friends > siblings
+# # YOU DO: Explain.
+
+# attendees = 9
+# meals = 8
+# puts attendees != meals
+# # YOU DO: Explain.
+
+
+# #-------------------
+# # PART 3: Logical Operators
+# #-------------------
+
+# is_hungry = true
+# finished_homework = false
+
+# # EXAMPLE:
+# # Determine if the user is hungry and has completed their homework
+# puts is_hungry && finished_homework
+# # Determine if the user is hungry or has completed their homework
+# puts is_hungry || finished_homework
+
+# loves_to_play = true
+# loves_dog_park = false
+# loves_treats = true
+# age = 1
+
+# # YOU DO:
+# # Determine if the dog loves to play and loves treats
+
+
+# # Determine if the dog loves to play and loves the dog park
+
+
+# # Determine if the dog loves to play or loves the dog park
+
+
+# # Determine if the dog loves to play and is a puppy
+
+# # What did your final line of code evaluate to? Why do you think that is? Explain.
+# # ANSWER:
