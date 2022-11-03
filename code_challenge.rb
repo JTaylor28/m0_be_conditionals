d4ddd1127c1c3868150f1ac22e6caee27be3edd3
James Taylor <jrtaylor0614@gmail.com>
2022-11-02 23:40:03 -0600

    Makes eveb odd code black

---
+++ code_challenge.rb
@@ -0,0 +1,16 @@
+# Introduce the challenge
+# - Demonstrate how to live-code the solution
+# - Explain each line of code (this can be done while live-coding or after)
+# - Show that the program works as intended by running the code (you should run the code more than once to prove it works for all logic paths)
+# - Share a challenge or question you had when you solved it the first time
+
+# Write a Ruby program that defines a variable that stores an Integer. 
+#The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+number = 5
+
+if number.even?
+    puts "even"
+else
+    puts "odd"
+end
